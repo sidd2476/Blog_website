306729b408b26c3f07c30f599688dfaaf76ade79
sidd2476 <siddhukanu1@gmail.com>
2020-05-15 23:22:26 +0530

    inital commit

--- app.js
+++ app.js
@@ -76,6 +76,13 @@ app.get("/contact", function(req, res){
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+// app.listen(3000, function() {
+//   console.log("Server started on port 3000");
+// });
+
+
+let port = process.env.PORT;
+if(port == null || port==""){
+  port = 3000;
+}
+app.listen(port);
